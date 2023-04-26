1ea67dc9ceabe9303cc2f750c23b50f8d871d2ee
ammarasifDSU <ammarasif9@gmail.com>
2023-04-26 16:17:27 +0400

    UI work

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FeatureContainer, FeatureButton } from './Feature';
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+      <h1>Deal of the Day</h1>
+      <p>Get Beef Burger and Mr Big Cheese</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+};
+
+export default Feature;
